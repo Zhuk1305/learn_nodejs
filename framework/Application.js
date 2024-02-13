f2b67e68ace4d97ef6dba5390a9f47a09c111c01
Zhuk1305 <vaneczhuk@gmail.com>
2024-02-13 22:46:41 +0100

    add middleware for parse url

--- framework/Application.js
+++ framework/Application.js
@@ -28,7 +28,6 @@ module.exports = class Application {
       Object.keys(endpoint).forEach((method) => {
         const handler = endpoint[method];
         this.emitter.on(this._getRouteMask(path, method), (req, res) => {
-          this.middlewares.forEach((middleware) => middleware(req, res));
           handler(req, res);
         });
       });
@@ -47,8 +46,9 @@ module.exports = class Application {
         if (body) {
           req.body = JSON.parse(body);
         }
+        this.middlewares.forEach((middleware) => middleware(req, res));
         const emitted = this.emitter.emit(
-          this._getRouteMask(req.url, req.method),
+          this._getRouteMask(req.pathname, req.method),
           req,
           res
         );
--- src/user-router.js
+++ src/user-router.js
@@ -8,6 +8,10 @@ const users = [
 ];
 
 router.get("/users", (req, res) => {
+  console.log(req.params.id);
+  if (req.params.id) {
+    return res.send(users.find((user) => user.id == req.params.id));
+  }
   res.send(users);
 });
 
